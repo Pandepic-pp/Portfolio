419d65b5dc97785774e64e975c771519fbfae23f
Pratyaksh <pratyaksh22official@gmail.com>
2025-03-16 10:50:49 +0530

    tracking corrected

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import { inject } from '@vercel/analytics/*';
+import { inject } from '@vercel/analytics';
 
 inject();
 platformBrowserDynamic()
